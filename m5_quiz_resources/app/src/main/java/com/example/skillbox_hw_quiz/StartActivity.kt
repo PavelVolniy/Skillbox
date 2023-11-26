6c115b246f187c4dc953f28d9e3c5d2efd88cf79
PavelVolniy <pavel.volniy.2020@gmail.com>
2023-11-27 00:47:36 +0300

    Homework 5.7 is ready

---
+++ m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.skillbox_hw_quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
